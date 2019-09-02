6e98f2f0f5f6f634d371175c52ef206d15960908
Nevynnyy <47993042+Nevynnyy@users.noreply.github.com>
2019-09-02 15:36:22 +0200

    Update map.c

--- hw0/map.c
+++ hw0/map.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int foo;                // a staticly allocated variable
+int foo;
 
-int recur(int i) {      // a recursive function
-    int j = i;          // a stack allocated variable within a recursive function
-    
-    printf("recur call %d: stack@ %lx\n", i, j); // TODO: fix this so it prints the address of j
+int recur(int i) {
+    int j = i;
+
+    printf("recur call %d: stack@ %lx\n", i, &j);
     if (i > 0) {
         return recur(i-1);
     }
     return 0;
 }
 
-int stuff = 7;          // a statically allocated, pre-initialized variable
+int stuff = 7;
 
 int main (int argc, char *argv[]) {
-    int i;                     // a stack allocated variable
-    char *buf1 = malloc(100);  // dynamically allocate some stuff
-    char *buf2 = malloc(100);  // and some more stuff...
-    
-    printf("_main  @ %lx\n", main);      // TODO: fix to print address of main
-    printf("recur @ %lx\n", recur);      // TODO: fix to print address of recur
-    printf("_main stack: %lx\n", i);     // TODO: fix to get address of the stack variable
-    printf("static data: %lx\n", stuff); // TODO: fix to get address of a static variable
-    
+    int i;
+    char *buf1 = malloc(100);
+    char *buf2 = malloc(100);
+
+    printf("_main  @ %lx\n", &main);
+    printf("recur @ %lx\n", &recur);
+    printf("_main stack: %lx\n", &i);
+    printf("static data: %lx\n", &stuff);
     printf("Heap: malloc 1: %lx\n", (long unsigned int) buf1);
     printf("Heap: malloc 2: %lx\n", (long unsigned int) buf2);
     recur(3);
