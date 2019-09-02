2243ccac96fb7e059a307028ac638f19f5ea4cc7
Nevynnyy <47993042+Nevynnyy@users.noreply.github.com>
2019-09-02 15:34:43 +0200

    Update arguments.c

--- hw0/arguments.c
+++ hw0/arguments.c
@@ -2,18 +2,30 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    // TODO: Print the program name
-    printf("Program name: %s\n", "REPLACE THIS");
-    
-    printf("Arguments: ");
-    // TODO: Print all the arguments given to the program.
-    //
-    //       Separate multiple arguments using commas on one line.
-    //       e.g. "Arguments: arg1,arg2,arg3"
-    //
-    //       Print "NONE" if no arguments are present.
-    /*COMPLETE HERE*/
-    printf("\n");
+    /*char*man[]={"qqq","we","wer"};
+    argv=man;
+    argc=2;*/
+    printf("Program name: %s\n", argv[0]);
+
+    if(argc==0){
+        printf("Arguments: NONE");
+    }else{
+       printf("Arguments: ");
+       int caunt=1;
+
+        while(caunt<argc+1){
+            if(caunt==argc){
+                printf("%s",argv[argc]);
+            }else{
+                printf("%s,",argv[caunt]);
+            }
+            ++caunt;
+        }
+
+        printf("\n");
+
+    }
+
 
     return 0;
 }
