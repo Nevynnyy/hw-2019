2fd688a11e0929b1487292b24bd4cb3f3f88c6b5
Nevynnyy <47993042+Nevynnyy@users.noreply.github.com>
2019-09-03 17:15:51 +0200

    Update wc.c

--- hw0/wc.c
+++ hw0/wc.c
@@ -2,35 +2,79 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-// total number of lines, words, chars (if multiple files are input)
+
 static int total_lines = 0, total_words = 0, total_chars = 0;
 void wc(FILE *infile, char *inname);
 
 int main(int argc, char *argv[]) {
     if(argc > 1) {
-        
-        // TODO: get the filename/s from argv, open each and pass it to wc
-        
-        if(argc > 2) { // print totals for multiple files
+
+        FILE* infile;
+        int caunt=1;
+        while(caunt<argc+1){
+            infile=fopen(argv[caunt],"r");
+            wc(infile,argv[caunt]);
+            ++caunt;
+        }
+
+        if(argc > 2) {
             printf("%7d %7d %7d   %s\n",  total_lines, total_words, total_chars, "total");
         }
-        
+
     } else {
-        // TODO: no arguments, process input from stdin
+
+        char input[20];
+        scanf("%s",input);
     }
-    
+
     return 0;
 }
 
 void wc(FILE *infile, char *inname) {
     int lines = 0, words = 0, chars = 0;
-    
-    // TODO: read the file character by character and count the
-    //       number of lines, words, and characters.
-    
-    total_lines += lines;
-    total_words += words;
-    total_chars += chars;
-    printf("%7d %7d %7d   %s\n",  lines, words, chars, inname);
+
+
+    if(infile==NULL){
+
+    }else{
+        char character;
+        /*int num=1;
+        char inp[10000];*/
+
+
+
+        character=getc(infile);
+        /*inp[0]=character;*/
+
+        while(character!=EOF){
+
+            if(character=='\n'){
+                ++lines;
+            }
+
+            /*if((inp[num-2]!=' ' || inp[num-2]!='\n' || inp[num-2]!='\t' || inp[num-2]!='\0') && (character==' ' || character=='\t' || character=='\0' || character=='\n')){
+                ++words;
+            }*/
+            int a=0,b=1,c;
+            if(character==' ' || character=='\t' || character=='\0' || character=='\n'){
+                ++words;
+            }
+
+
+            ++chars;
+            character=getc(infile);
+            /*inp[num]=character;
+            ++num;*/
+        }
+
+
+        total_lines += lines;
+        total_words += words;
+        total_chars += chars;
+        printf("%7d %7d %7d   %s\n",  lines, words, chars, inname);
+
+    }
+
 }
